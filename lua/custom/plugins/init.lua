0f9044602fba3c1f05e2592264b390959d55e14f
argon17 <premsagar0247@gmail.com>
2026-02-21 13:20:15 +0530

    feat: add github copilot plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,11 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  -- Find the plugins section and add:
+  {
+    'github/copilot.vim',
+    event = 'InsertEnter',
+    cmd = { 'Copilot' },
+  },
+}
